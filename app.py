b1cee2758ff7e848ba98a805a93a13c300123806
AyushMaji <ayushmaji2001@gmail.com>
2023-04-23 11:41:35 +0530

    update

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def maternal_health() :
         st.info("s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled")
     st.title(output)
 def fetal_health():
-    st.title('Fetal Health Prediction')
+    st.title('Fetal Health')
     st.write("s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
     st.header('Fetal Growth from 4 To 40 Weeks')
     st.image(Image.open('images/f1.jpg'), caption='Fetal Health Prediction')
